4921217c7f17479cf9e9e71cc7c5805833b4c15f
Tadam0 <triki.adam.lgf@gmail.com>
2025-12-19 09:43:50 +0000

    ajout d'un menu

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,9 @@ int main() {
   int nbrePlaces = 10;
   int choix; 
   cout << "************ Evaluation 2 ************" << endl << endl; 
+  cout << "1 - Entrée dans le parking\n"; 
+  cout << "2 - Nombre de places disponibles\n"; 
+  cout << "3 - Sortie du parking\n"; 
+  cout << "4 - Quitter\n"; 
   return 0;
 }
